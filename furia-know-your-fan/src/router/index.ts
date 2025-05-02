a839ed211ac904fea5bd1d00a31b649cee80385c
Felipe Schandler <felipeschandle@gmail.com>
2025-05-02 20:35:22 -0300

    Adjusting initial files

--- furia-know-your-fan/src/router/index.ts
+++ furia-know-your-fan/src/router/index.ts
@@ -1,22 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import FanQuestView from '@/rotas/FanQuestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'fanquest',
+      component: FanQuestView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    // Exemplo futuro (opcional)
+    // {
+    //   path: '/result',
+    //   name: 'result',
+    //   component: () => import('@/views/ResultView.vue'),
+    // },
   ],
 })
 
