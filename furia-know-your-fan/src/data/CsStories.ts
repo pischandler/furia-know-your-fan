c77f65dfb182ad7127fc2209614dc41d45d28c10
Felipe Schandler <felipeschandle@gmail.com>
2025-05-02 20:36:43 -0300

    Story Videos href function

---
+++ furia-know-your-fan/src/data/CsStories.ts
@@ -0,0 +1,14 @@
+export const csStories = [
+  {
+    video: new URL('@/assets/videos/cs/cs_story_1.mp4', import.meta.url).href,
+  },
+  {
+    video: new URL('@/assets/videos/cs/cs_story_2.mp4', import.meta.url).href,
+  },
+  {
+    video: new URL('@/assets/videos/cs/cs_story_3.mp4', import.meta.url).href,
+  },
+  {
+    video: new URL('@/assets/videos/cs/cs_story_4.mp4', import.meta.url).href,
+  },
+]
---
+++ furia-know-your-fan/src/data/LolStories.ts
@@ -0,0 +1,11 @@
+export const lolStories = [
+  {
+    video: new URL('@/assets/videos/lol/lol_story_1.mp4', import.meta.url).href,
+  },
+  {
+    video: new URL('@/assets/videos/lol/lol_story_2.mp4', import.meta.url).href,
+  },
+  {
+    video: new URL('@/assets/videos/lol/lol_story_3.mp4', import.meta.url).href,
+  },
+]
