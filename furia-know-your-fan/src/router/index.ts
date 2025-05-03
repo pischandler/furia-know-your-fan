b96acc4bb43cb467ac44dcc0886b1c48b872c01b
Felipe Schandler <felipeschandle@gmail.com>
2025-05-03 00:23:39 -0300

    Adjusting game routes and file's names

--- furia-know-your-fan/src/router/index.ts
+++ furia-know-your-fan/src/router/index.ts
@@ -1,21 +1,20 @@
+// src/router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
-import FanQuestView from '@/rotas/FanQuestView.vue'
+// import LoginView from '@/routes/LoginView.vue'
+// import FormStep1 from '@/routes/FormStep1.vue'
+// import FormStep2 from '@/routes/FormStep2.vue'
+import GameStart from '@/routes/GameStart.vue'
+
+const routes = [
+  // { path: '/', name: 'login', component: LoginView },
+  // { path: '/form/register', name: 'form-reg', component: FormStep1 },
+  // { path: '/form/document', name: 'form-doc', component: FormStep2 },
+  { path: '/game', name: 'game', component: GameStart },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'fanquest',
-      component: FanQuestView,
-    },
-    // Exemplo futuro (opcional)
-    // {
-    //   path: '/result',
-    //   name: 'result',
-    //   component: () => import('@/views/ResultView.vue'),
-    // },
-  ],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
