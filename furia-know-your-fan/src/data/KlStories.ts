122760a1326cba97fa0aef8e7eb60f81150ce9c5
Felipe Schandler <felipeschandle@gmail.com>
2025-05-04 14:23:25 -0300

    Kings League game assets

--- furia-know-your-fan/src/data/KlStories.ts
+++ furia-know-your-fan/src/data/KlStories.ts
@@ -1,11 +1,11 @@
 export const klStories = [
   {
-    video: new URL('@/assets/videos/kl/kl_story_1.mp4', import.meta.url).href,
+    video: new URL('@/assets/videos/kl/story/kl_story_1.mp4', import.meta.url).href,
   },
   {
-    video: new URL('@/assets/videos/kl/kl_story_2.mp4', import.meta.url).href,
+    video: new URL('@/assets/videos/kl/story/kl_story_2.mp4', import.meta.url).href,
   },
   {
-    video: new URL('@/assets/videos/kl/kl_story_3.mp4', import.meta.url).href,
+    video: new URL('@/assets/videos/kl/story/kl_story_3.mp4', import.meta.url).href,
   },
 ]
