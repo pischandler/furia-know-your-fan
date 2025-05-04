1def7d05013a0284f2e74a577cdcd0d18d474371
Felipe Schandler <felipeschandle@gmail.com>
2025-05-04 16:56:04 -0300

    Adjusting Game page to receive more questions

--- furia-know-your-fan/src/routes/GameStart.vue
+++ furia-know-your-fan/src/routes/GameStart.vue
@@ -9,18 +9,33 @@ import CsQuestion4 from '@/components/questions/cs/CsQuestion4.vue'
 import LolQuestion1 from '@/components/questions/lol/LolQuestion1.vue'
 import LolQuestion2 from '@/components/questions/lol/LolQuestion2.vue'
 import LolQuestion3 from '@/components/questions/lol/LolQuestion3.vue'
+import KlQuestion1 from '@/components/questions/kl/KlQuestion1.vue'
+import KlQuestion2 from '@/components/questions/kl/KlQuestion2.vue'
+import KlQuestion3 from '@/components/questions/kl/KlQuestion3.vue'
+import KlQuestion4 from '@/components/questions/kl/KlQuestion4.vue'
+import KlQuestion5 from '@/components/questions/kl/KlQuestion5.vue'
+import KlQuestion6 from '@/components/questions/kl/KlQuestion6.vue'
 import { lolStories } from '@/data/LolStories'
 import { klStories } from '@/data/KlStories'
+import UserFormQuestions from '@/components/UserFormQuestions.vue'
 
 const phaseIndex = ref(0)
 const currentQuestion = ref(1)
+
+const questionPhases = {
+  1: 4, 
+  3: 3, 
+  5: 6, //TODO: componente estatico - ajustar para aceitar quantas perguntas forem
+}
 
 function handleStoriesEnd() {
   phaseIndex.value++
 }
 
 function handleAnswer(selected: string | string[]) {
-  if (currentQuestion.value < 4) {
+  const totalQuestions = questionPhases[phaseIndex.value] || 0
+
+  if (currentQuestion.value < totalQuestions) {
     currentQuestion.value++
   } else {
     currentQuestion.value = 1
@@ -48,4 +63,15 @@ function handleAnswer(selected: string | string[]) {
   </template>
 
   <StoryPlayer v-else-if="phaseIndex === 4" :stories="klStories" @end="handleStoriesEnd" />
+
+  <template v-else-if="phaseIndex === 5">
+    <KlQuestion1 v-if="currentQuestion === 1" @answered="handleAnswer" />
+    <KlQuestion2 v-else-if="currentQuestion === 2" @answered="handleAnswer" />
+    <KlQuestion3 v-else-if="currentQuestion === 3" @answered="handleAnswer" />
+    <KlQuestion4 v-else-if="currentQuestion === 4" @answered="handleAnswer" />
+    <KlQuestion5 v-else-if="currentQuestion === 5" @answered="handleAnswer" />
+    <KlQuestion6 v-else-if="currentQuestion === 6" @answered="handleAnswer" />
+  </template>
+
+  <UserFormQuestions v-else-if="phaseIndex === 6" />
 </template>
